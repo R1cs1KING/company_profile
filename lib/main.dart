1bc4039711208fe4ecdb1c01e3fe64088b9c5c2d
richardtarnoczi <ricsi960413@gmail.com>
2018-08-07 12:34:59 +0200

    company_profile
    Push company_profile

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:company_profile/ui/company_details_animator.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new CompanyDetailsAnimator(),
+    );
+  }
+}
